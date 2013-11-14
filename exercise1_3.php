93030507863b09ade9ddfeb29edd8aec8b90ede0
com02-03 <com02-03@com02-03-PC>
2013-11-14 14:24:38 +0700

    Nov 14 2013

---
+++ exercise1_3.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+    <title>Exercise 1_3</title>
+     <style type="text/css">
+            body{
+                font-family: Helvetica;
+                font-size: 16pt;
+            }
+        </style>
+    </head>
+    <body>
+<?php
+$x=10;
+$y=20;
+
+//echo $y += ++$x;
+echo $y +=$x++;
+ ?>
+    </body>
+</html>
